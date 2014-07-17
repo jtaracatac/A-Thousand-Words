af18f9bcf08865393ef5d7ffa33ee03cdf8da37a
Isabelle Jolene Taracatac <Jolene@Isabelles-MacBook-Pro.local>
2014-07-16 22:13:05 -0400

    Initial Commit

---
+++ Thousand Words/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Thousand Words
+//
+//  Created by Isabelle Jolene Taracatac on 7/16/14.
+//  Copyright (c) 2014 Code Coallition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TWAppDelegate class]));
+    }
+}
